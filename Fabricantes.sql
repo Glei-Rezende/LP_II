2d73374d59650e3a8a41e9940951f83ea6c38f0d
Gleialison <gleialison@gmail.com>
2026-06-26 14:42:30 -0300

    Projeto LP II

---
+++ Fabricantes.sql
@@ -0,0 +1,4 @@
+INSERT INTO fabricante (nome, pais_origem) VALUES ('Fiat', 'Itália');
+INSERT INTO fabricante (nome, pais_origem) VALUES ('Toyota', 'Japão');
+INSERT INTO fabricante (nome, pais_origem) VALUES ('Honda', 'Japão');
+INSERT INTO fabricante (nome, pais_origem) VALUES ('GM', 'EUA');
